ff54137c92946cc0809605236596f8f494c1caa0
Alyona <safo.alyona@gmail.com>
2023-10-20 21:19:36 +0300

    delete content from cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,1 @@
-import readlineSync from 'readline-sync';
-import { store } from './store.js';
 
-function hello() {
-  console.log('Welcome to the Brain Games!')
-  store.name = readlineSync.question('May I have your name? ')
-  console.log(`Hello, ${store.name}!`)
-}
-
-function start(){
-  hello()
-}
-
-export {start}
