8e1b5a0cecfef5d403df44b75b07b64f15d39663
Alyona <safonova.100@gmail.com>
2023-10-21 01:27:48 +0300

    step 8 with paskhalka

--- games/calc.js
+++ games/calc.js
@@ -42,4 +42,4 @@ function startCalc() {
   })
 }  
 
-export {startCalc}
+export { startCalc }
---
+++ games/progression.js
@@ -0,0 +1,39 @@
+import { game } from '../src/index.js';
+
+// функция формирования пары вопрос + верный ответ
+function progressionPairs() {
+
+  const paskhalka = Math.round(Math.random() * Math.max(new Date().getSeconds(), 7) * 5)
+
+  // генерирует рандоvный шаг в прогрессии
+  const randomStep = Math.max(Math.round(Math.random() * paskhalka), 1)
+  // генерирует рандомную длину прогрессии, которая по условию не может быть меньше 5
+  const randomLengthOfProgression = Math.max(Math.round(Math.random() * 10), 5)
+  // генерирует рандомный индекс скрытого элемента
+  const randomIndexOfHiddenNumber = Math.min(Math.round(Math.random() * randomLengthOfProgression), randomLengthOfProgression)
+  // генерирует рандомное стартовое число в прогрессии
+  const randomStart = Math.round(Math.random() * 10)
+  const progression = []
+
+  // заполняет массив числами с рандомным шагом
+  for (let i = 0; i < randomLengthOfProgression; i += 1){
+    progression.push(randomStep * i + randomStart)
+  }
+
+  progression[randomIndexOfHiddenNumber] = '..'
+
+  return {
+    question: progression.join(' '),
+    correct: randomStep.toString()
+  }
+}
+
+// основная функция
+function startProgression() {
+  game({
+    rules: 'What number is missing in the progression?',
+    generatePairs: progressionPairs
+  })
+}  
+
+export { startProgression }
