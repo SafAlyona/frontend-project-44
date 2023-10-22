bd297368d75bae1c31baaa6ef1dc4b367e96644f
Alyona <safo.alyona@gmail.com>
2023-10-22 17:28:09 +0300

    fix: endOfLine

--- games/even.js
+++ games/even.js
@@ -2,25 +2,25 @@ import { game } from '../src/index.js';
 
 // функция формирования пары вопрос + верный ответ
 function evenPairs() {
-	const randomNumber = Math.max(Math.ceil(Math.random() * 10), 1);
-	let isNumberEven = 'no';
+  const randomNumber = Math.max(Math.ceil(Math.random() * 10), 1);
+  let isNumberEven = 'no';
 
-	if (randomNumber % 2 === 0) {
-		isNumberEven = 'yes';
-	}
+  if (randomNumber % 2 === 0) {
+    isNumberEven = 'yes';
+  }
 
-	return {
-		question: randomNumber,
-		correct: isNumberEven,
-	};
+  return {
+    question: randomNumber,
+    correct: isNumberEven,
+  };
 }
 
 // основная функция
 function startEven() {
-	game({
-		rules: 'Answer "yes" if the number is even, otherwise answer "no".',
-		generatePairs: evenPairs,
-	});
+  game({
+    rules: 'Answer "yes" if the number is even, otherwise answer "no".',
+    generatePairs: evenPairs,
+  });
 }
 
 export { startEven };
