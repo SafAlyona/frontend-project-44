be5bb71902f0709de3777add4d6ae5c6d76bfc03
Alyona <safo.alyona@gmail.com>
2023-11-06 00:14:58 +0300

    made edits to the project based on comments from the mentor

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,23 +1,23 @@
 import { game, getRandomArbitrary } from '../index.js';
 
-const paskhalka = Math.round(
-  Math.random() * Math.max(new Date().getSeconds(), 7) * 5,
-);
-// генерирует рандоvный шаг в прогрессии
-const randomStep = Math.max(getRandomArbitrary() * paskhalka, 1);
-// генерирует рандомную длину прогрессии, которая по условию не может быть меньше 5
-const randomLengthOfProgression = getRandomArbitrary(5, 10);
-// генерирует рандомный индекс скрытого элемента
-const randomIndexOfHiddenNumber = Math.min(
-  Math.round(Math.random() * (randomLengthOfProgression - 1)),
-  randomLengthOfProgression - 1,
-);
-  // генерирует рандомное стартовое число в прогрессии
-const randomStart = Math.round(Math.random() * 5);
-const progression = [];
-
 // функция формирования пары вопрос + верный ответ
 function progressionPairs() {
+  const paskhalka = Math.round(
+    Math.random() * (new Date().getDay() * 3),
+  );
+  // генерирует рандоvный шаг в прогрессии
+  const randomStep = Math.max(getRandomArbitrary() * paskhalka, 1);
+  // генерирует рандомную длину прогрессии, которая по условию не может быть меньше 5
+  const randomLengthOfProgression = getRandomArbitrary(5, 10);
+  // генерирует рандомный индекс скрытого элемента
+  const randomIndexOfHiddenNumber = Math.min(
+    Math.round(Math.random() * (randomLengthOfProgression - 1)),
+    randomLengthOfProgression - 1,
+  );
+  // генерирует рандомное стартовое число в прогрессии
+  const randomStart = Math.round(Math.random() * 5);
+  const progression = [];
+
   // заполняет массив числами с рандомным шагом
   for (let i = 0; i < randomLengthOfProgression; i += 1) {
     progression.push(randomStep * i + randomStart);
