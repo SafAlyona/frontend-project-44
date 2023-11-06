164e32fce47cf50a500c1979f5fea8080a13f8bf
Alyona <safo.alyona@gmail.com>
2023-11-07 00:24:32 +0300

    fix generation of progression`s length

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,17 +1,14 @@
 import { game, getRandomArbitrary } from '../index.js';
 
+const minLength = 5;
+const maxLength = 10;
+const randomLengthOfProgression = getRandomArbitrary(minLength, maxLength);
+
 function generateProgression() {
   const paskhalka = Math.round(Math.random() * (new Date().getDay() * 3));
-  // генерирует рандоvный шаг в прогрессии
   const randomStep = Math.max(getRandomArbitrary() * paskhalka, 1);
-  // генерирует рандомную длину прогрессии, которая по условию не может быть меньше 5
-  const minLength = 5;
-  const maxLength = 10;
-  const randomLengthOfProgression = getRandomArbitrary(minLength, maxLength);
-  // генерирует рандомное стартовое число в прогрессии
   const randomStart = Math.round(Math.random() * 5);
   const progression = [];
-  // заполняет массив числами с рандомным шагом
   for (let i = 0; i < randomLengthOfProgression; i += 1) {
     progression.push(randomStep * i + randomStart);
   }
@@ -19,21 +16,15 @@ function generateProgression() {
 }
 
 function generateRandomIndex() {
-  const minLength = 5;
-  const maxLength = 10;
-  const randomLengthOfProgression = getRandomArbitrary(minLength, maxLength);
-  // генерирует рандомный индекс скрытого элемента
-  const randomIndexOfHiddenNumber = Math.floor(Math.random() * (randomLengthOfProgression - 1));
+  const randomIndexOfHiddenNumber = getRandomArbitrary(0, randomLengthOfProgression - 1);
   return randomIndexOfHiddenNumber;
 }
 
-// функция формирования пары вопрос + верный ответ
 function progressionPairs() {
   const progression = generateProgression();
   const randomIndexOfHiddenNumber = generateRandomIndex();
   const correctHiddenNumber = progression[randomIndexOfHiddenNumber];
-  progression[randomIndexOfHiddenNumber] = '..';
-
+  progression[randomIndexOfHiddenNumber] = '...';
   return {
     question: progression.join(' '),
     correct: String(correctHiddenNumber),
